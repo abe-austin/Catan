6823af2621c7b49dd14dd8c79fa28a46949ef48d
Cory <cjkirk09@gmail.com>
2014-07-01 19:03:08 -0600

    has serverProxy package

---
+++ java/src/client/serverProxy/ServerPoller.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package client.serverProxy;
+
+/**
+ *
+ * @author Cory
+ */
+public class ServerPoller {
+    private ServerProxyFacade serverProxy;  
+    private GameModel gameModel;
+    /**
+     * 
+     */
+    public ServerPoller(){}
+    public setServerProxy( ServerProxyFacade serverProxy){}
+    public sendGameModel(GameModel gameModel){}
+    public poll(){}
+
+}
