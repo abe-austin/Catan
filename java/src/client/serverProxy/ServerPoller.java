623c7cc346ed7fa0eef52684c727f88af2e6cbe6
Cory <cjkirk09@gmail.com>
2014-07-01 19:08:19 -0600

    "added java doc to server poller"

--- java/src/client/serverProxy/ServerPoller.java
+++ java/src/client/serverProxy/ServerPoller.java
@@ -14,11 +14,22 @@ public class ServerPoller {
     private ServerProxyFacade serverProxy;  
     private GameModel gameModel;
     /**
-     * 
+     * empty constructor
      */
     public ServerPoller(){}
+    /**
+     * sets the server proxy that it will connect to
+     * @param serverProxy  the serverProxy
+     */
     public setServerProxy( ServerProxyFacade serverProxy){}
+    /**
+     * sends the current game model to the model
+     * @param gameModel the current gameModel received from the serverProxy
+     */
     public sendGameModel(GameModel gameModel){}
+    /**
+     * polls the server to see if there have been changes
+     */
     public poll(){}
 
 }
