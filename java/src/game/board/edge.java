d78c2a7014f77221b1515cbee2057b96da51fc8b
Abe Austin <abe.austin@gmail.com>
2014-07-02 12:29:59 -0600

    Updated edge to be beautiful

--- java/src/game/board/edge.java
+++ java/src/game/board/edge.java
@@ -1,12 +1,42 @@
 package game.board;
 
 import java.util.List;
+
+import shared.locations.EdgeLocation;
 import game.pieces.BoardPiece;
 
 public class edge {
 	
+	private EdgeLocation loc;
 	private List<hexTile> tilesBelongedTo;
-	BoardPiece builtStructure;//null if nothing built here yet
+	BoardPiece builtStructure;
 	
+	public edge(EdgeLocation loc, List<hexTile> tilesBelongedTo) {
+		this.loc = loc;
+		this.tilesBelongedTo = tilesBelongedTo;
+		builtStructure = null;		
+	}
+	
+	/**
+	 * @return whether there has been an object built here already
+	 */
+	public boolean hasStructure()
+	{
+		return(builtStructure != null);
+	}
+	
+	/**
+	 * @param road the game piece that is being placed on this edge
+	 */
+	public void buildStructure(BoardPiece road){
+		builtStructure = road;
+	}
+	
+	/**
+	 * @return the tiles that share this edge
+	 */
+	public List<hexTile> getConnectedHexes(){
+		return tilesBelongedTo;
+	}
 	
 }
