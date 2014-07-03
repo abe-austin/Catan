601f59e992fc8fee24eb6faaba40b7770a1f05c7
Candice Davis <candicedavis222@gmail.com>
2014-07-02 21:38:04 -0600

    merge conflicts

--- java/src/shared/communication/ClientCommunicator.java
+++ java/src/shared/communication/ClientCommunicator.java
@@ -2,4 +2,15 @@ package shared.communication;
 
 public class ClientCommunicator {
 	
+	public ClientCommunicator() {
+		
+	}
+	
+	public void doPost() {
+		
+	}
+	
+	public void doGet() {
+		
+	}
 }
