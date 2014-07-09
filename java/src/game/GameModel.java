62db235cad1a5fa6912169ab0712443ab85e1ae3
Brent <brentjoeroberts@gmail.com>
2014-07-08 18:20:53 -0600

    Updated GameModel.java and added TadeOffer.java

--- java/src/game/GameModel.java
+++ java/src/game/GameModel.java
@@ -15,6 +15,12 @@ public class GameModel {
     private BoardModel board;
     private Set<Player> players;
     private Bank bank;
+    private ChatLog chatLog;
+    private GameHistory gameHistory;
+    private TurnTracker turnTracker;
+    private TradeOffer tradeOffer;
+    private int version;
+    private int winner;
 
     public GameModel() {
         board = new BoardModel();
@@ -65,5 +71,62 @@ public class GameModel {
     public Bank getBank() {
         return bank;
     }
+
+	public ChatLog getChatLog() {
+		return chatLog;
+	}
+
+	public void setChatLog(ChatLog chatLog) {
+		this.chatLog = chatLog;
+	}
+
+	public GameHistory getGameHistory() {
+		return gameHistory;
+	}
+
+	public void setGameHistory(GameHistory gameHistory) {
+		this.gameHistory = gameHistory;
+	}
+
+	public TurnTracker getTurnTracker() {
+		return turnTracker;
+	}
+
+	public void setTurnTracker(TurnTracker turnTracker) {
+		this.turnTracker = turnTracker;
+	}
+
+	public TradeOffer getTradeOffer() {
+		return tradeOffer;
+	}
+
+	public void setTradeOffer(TradeOffer tradeOffer) {
+		this.tradeOffer = tradeOffer;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public int getWinner() {
+		return winner;
+	}
+
+	public void setWinner(int winner) {
+		this.winner = winner;
+	}
+
+	public void setPlayers(Set<Player> players) {
+		this.players = players;
+	}
+
+	public void setBank(Bank bank) {
+		this.bank = bank;
+	}
+    
     
 }
