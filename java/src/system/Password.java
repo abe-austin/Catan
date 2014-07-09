6563f992327642b0db6a5eff51d30fac516c2827
Kevin MacMaster <kmac0443@gmail.com>
2014-07-08 18:02:52 -0600

    User, System

---
+++ java/src/system/Password.java
@@ -0,0 +1,23 @@
+package system;
+
+/**
+ *
+ * @author Kevin MacMaster
+ */
+public class Password {
+    private String password;
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ java/src/system/User.java
@@ -0,0 +1,54 @@
+package system;
+
+/**
+ * @author Kevin MacMaster
+ */
+public class User {
+    private Username username;
+    private Password password;
+    private int id;
+
+    /**
+     * @return the username
+     */
+    public Username getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(Username username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public Password getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(Password password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+}
