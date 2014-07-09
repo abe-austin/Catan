53b652bdcbe36c1c066ac93482331d1c5f9896d6
Candice Davis <candicedavis222@gmail.com>
2014-07-08 18:05:38 -0600

    added PointsTest

--- java/test/player/PlayerTest.java
+++ java/test/player/PlayerTest.java
@@ -1,14 +1,63 @@
 package player;
 
 import static org.junit.Assert.*;
+import game.cards.DevelopmentCard;
+import game.cards.SpecialCard;
+import game.pieces.BoardPiece;
 
 import org.junit.Test;
+
+import player.Player;
+import shared.definitions.CatanColor;
+import shared.definitions.DevCardType;
+import shared.definitions.PieceType;
+import shared.definitions.SpecialCardType;
 
 public class PlayerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetAvaliableBoardPiece(PieceType type) {
+
 	}
 
+	@Test
+	public void testHasAvaiableBoardPiece(PieceType type) {
+
+	}
+
+	@Test
+	public void testGiveDevelopmentCard(DevCardType devCardType) {
+
+	}
+
+	@Test
+	public void testGiveSpecialCard(SpecialCardType specialCardType) {
+
+	}
+
+	@Test
+	public void testAddSpecialCard(SpecialCard specialCard) {
+
+	}
+
+	@Test
+	public void testAddPoint() {
+
+	}
+
+	@Test
+	public void testGetSoldiersPlayed() {
+
+	}
+
+	@Test
+	public int testGetHandSize() {
+
+	}
+
+	@Test
+	public boolean testEquals(Object other) {
+
+	}
 }
+
