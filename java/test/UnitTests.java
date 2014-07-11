1763bb87951d0fc3092b646af1b24c3a0314d325
Candice Davis <candicedavis222@gmail.com>
2014-07-10 18:43:45 -0600

    added UnitTests

---
+++ java/test/UnitTests.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.*
+
+public class UnitTests {
+	
+	@Before
+	public void setup() {
+	}
+
+	@After
+	public void teardown() {
+	}
+	
+	@Test
+	public void test_1() {
+		assertEquals("OK", "OK");
+		assertTrue(true);
+		assertFalse(false);
+	}
+	
+	public static void main(String[] args) {
+		
+		String[] testClasses = new String[] {
+			"client.ServerAccessTest",
+			"client.ServerPollerTest",
+			"game.bank.BankTest",
+			"game.cards.CardOwnerTest",
+			"game.cards.DevelopmentCardTest",
+			"game.cards.ResourceCardTest",
+			"game.cards.SpecialCardTest",
+			"game.pieces.ChatLogTest",
+			"game.pieces.GameHistoryTest",
+			"game.pieces.GameModelTest",
+			"game.pieces.TradeOfferTest",
+			"game.pieces.TurnTrackerTest",
+			"player.PlayerTest",
+			"player.PointsTest"
+		};	
+		
+		org.junit.runner.JUnitCore.(testClasses);
+	}
+}
