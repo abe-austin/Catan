037274b088212bc27d9cea1415d4dfb6c6c0679e
Candice Davis <candicedavis222@gmail.com>
2014-07-10 18:10:54 -0600

    pointsTest

---
+++ java/test/player/PointsTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import player.Player;
+
+
+public class PointsTest {
+
+	@Test
+	  public void testPoints() {
+		Player player = new Player(Red.color, User new User());
+		
+		//test addPoint
+		player.getPoints().addPoint();
+		assertEquals(1, player.getPoints());
+		
+		//test subTractPoint
+		player.getPoints().subtractPoint();
+		assertEquals(0, player.getPoints());
+      }
+	
+	@Test
+      public boolean testIsVictory() {
+		Player player = new Player(Red.color, User new User());
+		
+		//not victorious 
+		
+		//victorious
+		
+      }
+
+}
