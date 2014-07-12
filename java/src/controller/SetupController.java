ca2b08777ce407dcb2b1e0e9b21ade3b583d7758
Candice Davis <candicedavis222@gmail.com>
2014-07-11 19:06:10 -0600

    updated setUpController

--- java/src/controller/SetupController.java
+++ java/src/controller/SetupController.java
@@ -1,108 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package controller;
 
+import player.Player;
 import shared.definitions.CatanColor;
+import system.User;
 import client.data.GameInfo;
+import client.serverProxy.ServerProxyFacade;
 
 /**
  * JoinGameController, PlayerWaitingController, LoginController
  */
 class SetupController {
 	
+	ServerProxyFacade serverProxyFacade;
+	
 	public SetupController() {
-		
+		serverProxyFacade = new ServerProxyFacade(true);
 	}
     
     /**
 	 * Displays the join game view
 	 */
-	void startJoinGame(){//JoinGameController --goes in Setup !!Not sure if needed
+	void joingGameStart(){} //JoinGameController --goes in Setup !!Not sure if needed
             
-        }
-	
 	/**
 	 * Called by the join game view when the user clicks "Create new game" button.
 	 * Displays the new game view.
 	 */
-	void startCreateNewGame(){//JoinGameController --goes in Setup !!Not sure if needed
+	void startCreateNewGame(){} //JoinGameController --goes in Setup !!Not sure if needed
             
-        }
-	
 	/**
 	 * Called by the new game view when the user clicks the "Cancel" button
 	 */
-	void cancelCreateNewGame(){//JoinGameController --goes in Setup !!Not sure if needed
-            
-        }
-	
+	void cancelCreateNewGame(){} //JoinGameController --goes in Setup !!Not sure if needed
+ 
 	/**
 	 * Called by the new game view when the user clicks the "Create Game" button
 	 */
-	void createNewGame(){//JoinGameController --goes in Setup !!Not sure if needed
+	void createNewGame(){} //JoinGameController --goes in Setup !!Not sure if needed
             
-        }
-	
 	/**
 	 * Called by the join game view when the user clicks a "Join" or "Re-join" button.
 	 * Displays the select color view.
 	 * 
 	 * @param game The game that the user is joining
 	 */
-	void startJoinGame(GameInfo game){//JoinGameController --goes in Setup !!Not sure if needed
+	void startJoingGame(GameInfo game){} //JoinGameController --goes in Setup !!Not sure if needed
             
-        }
-	
 	/**
 	 * Called by the select color view when the user clicks the "Cancel" button
 	 */
-	void cancelJoinGame(){//JoinGameController --goes in Setup !!Not sure if needed
-            
-        }
-	
+	void cancelJoinGame(){} //JoinGameController --goes in Setup !!Not sure if needed
+           
 	/**
 	 * Called by the select color view when the user clicks the "Join Game" button
 	 * 
 	 * @param color The color selected by the user
 	 */
-	void joinGame(CatanColor color){//JoinGameController --goes in Setup
-            
-        }
+	void joinGame(CatanColor color, User user){ //JoinGameController --goes in Setup
+            serverProxyFacade.joinGame(user.getId(), color);
+    }
+	
         /**
 	 * Displays the player waiting view
 	 */
-	void startPlayerWaiting(){//PlayerWaitingController --goes in Setup !!Not sure if needed
+	void playerWaitingStart(){} //PlayerWaitingController --goes in Setup !!Not sure if needed
             
-        }
-	
 	/**
 	 * Called when the "Add AI" button is clicked in the player waiting view
 	 */
-	void addAI(){//PlayerWaitingController --goes in Setup
-            
-        }
+	void addAI(String AIType){ //PlayerWaitingController --goes in Setup
+		serverProxyFacade.addAI(AIType);
+    }
+	
         /**
 	 * Displays the login view
 	 */
-	void startLogin(){//LoginController --goes in Setup !!Not sure if needed
+	void loginStart(){} //LoginController --goes in Setup !!Not sure if needed
             
-        }
-	
 	/**
 	 * Called when the user clicks the "Sign in" button in the login view
 	 */
-	void signIn(){//LoginController --goes in Setup
-            
-        }
+	void signIn(String username, String password){ //LoginController --goes in Setup
+		serverProxyFacade.loginUser(username, password);
+    }
 	
 	/**
 	 * Called when the user clicks the "Register" button in the login view
 	 */
-	void register(){//LoginController --goes in Setup
-            
-        }
+	void register(String username, String password){ //LoginController --goes in Setup
+		serverProxyFacade.registerUser(username, password);
+    }
 }
