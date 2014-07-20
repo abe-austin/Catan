d8404819c298d32450438a5125380e51b4d7d0fe
Brent <brentjoeroberts@gmail.com>
2014-07-19 18:15:41 -0600

    gameModelChanged for chat log

--- java/src/client/communication/ChatController.java
+++ java/src/client/communication/ChatController.java
@@ -1,6 +1,10 @@
 package client.communication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import client.base.*;
+import client.parse.ParsedChat;
 import controller.ControllerFacade;
 import controller.IControllerFacadeListener;
 import game.GameModel;
@@ -11,10 +15,13 @@ import game.GameModel;
  */
 public class ChatController extends Controller implements IChatController, IControllerFacadeListener {
 
+	private IChatView view;
+	
 	public ChatController(IChatView view) {
 		
 		super(view);
-                ControllerFacade.getSingleton().addListener(this);
+        ControllerFacade.getSingleton().addListener(this);
+        this.view = view;        
 	}
 
 	@Override
@@ -26,10 +33,16 @@ public class ChatController extends Controller implements IChatController, ICont
 	public void sendMessage(String message) {
 		ControllerFacade.getSingleton().sendMessage(message);
 	}
-        @Override
-        public void gameModelChanged(GameModel gameModel){
-        
-        }
+	
+    @Override
+    public void gameModelChanged(GameModel gameModel){
+    	List<LogEntry> entries = new ArrayList<LogEntry>();
+    	for(ParsedChat mes : gameModel.getGameHistory().getChatlog().getChatLines()) {
+    		LogEntry entry = new LogEntry(shared.definitions.CatanColor.BLUE, mes.getMessage());
+    		entries.add(entry);
+    	}
+    	view.setEntries(entries);
+    }
 
 }
 
