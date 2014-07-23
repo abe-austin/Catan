8c3496360c580213df361604d536e46e3be2aecc
Kevin MacMaster <kmac0443@gmail.com>
2014-07-22 19:59:23 -0600

    Roll Timer Implementation

--- java/src/client/roll/RollController.java
+++ java/src/client/roll/RollController.java
@@ -4,6 +4,8 @@ import client.base.*;
 import controller.ControllerFacade;
 import controller.IControllerFacadeListener;
 import game.GameModel;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
@@ -11,6 +13,9 @@ import game.GameModel;
  */
 public class RollController extends Controller implements IRollController, IControllerFacadeListener {
 
+	private Timer timer;
+	private boolean waitingOnTimer = false;
+    
 	private IRollResultView resultView;
 
 	/**
@@ -29,7 +34,8 @@ public class RollController extends Controller implements IRollController, ICont
         
 	@Override
         public void gameModelChanged(GameModel gameModel){
-            // NO CHANGE
+            if(resultView.isModalShowing() && !waitingOnTimer)
+               startRolling();
         }
          
 	
@@ -48,7 +54,42 @@ public class RollController extends Controller implements IRollController, ICont
 	public void rollDice() {
                 getResultView().setRollValue(ControllerFacade.getSingleton().rollDice());
 		getResultView().showModal();
+                waitingOnTimer = false;
+                timer.cancel();
+	}
+        
+        private void startRolling() {
+            if (!waitingOnTimer) {
+                this.timer = new Timer();
+                TimerTask task = new DisplayCountdown();
+                timer.schedule(task, 0, 1000);
+                getRollView().showModal();
+                waitingOnTimer = true;
+            }
 	}
 
-}
+	public int pollRoll(int seconds) {
+            if (seconds > 0) {
+                getRollView().setMessage(seconds + " seconds left to roll");
+                seconds--;
+            } else {
+                getRollView().closeModal();
+                rollDice();
+            }
+            
+            return seconds;
+	}
 
+	class DisplayCountdown extends TimerTask {
+            int seconds;
+
+            public DisplayCountdown() {
+                seconds = 10;
+            }
+
+            @Override
+            public void run() {
+                seconds = pollRoll(seconds);
+            }
+	}
+}
