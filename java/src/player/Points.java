c798fa1e071ca7ee3b0a5a0508e578bc2212ac36
Candice Davis <candicedavis222@gmail.com>
2014-07-30 18:33:31 -0600

    player and points

--- java/src/player/Points.java
+++ java/src/player/Points.java
@@ -5,42 +5,41 @@ package player;
  * @author Kevin MacMaster
  */
 public class Points {
-        private int points;
+	private int points;
 
-        public Points() {
-            points = 0;
-        }
+	public Points() {
+		points = 0;
+	}
 
-        /**
-         * adds a single point
-         * @post player now has one more point
-         */
-        public void addPoint() {
-            points++;
-        }
+	/**
+	 * adds a single point
+	 * @post player now has one more point
+	 */
+	public void addPoint() {
+		points++;
+	}
 
-        /**
-         * subtracts a single point
-         * @post player now has one less point
-         */
-        public void subtractPoint() {
-            points--;
-        }
+	/**
+	 * subtracts a single point
+	 * @post player now has one less point
+	 */
+	public void subtractPoint() {
+		points--;
+	}
 
-        /**
-         *
-         * @return current points
-         */
-        public int getPoints() {
-            return points;
-        }
+	/**
+	 *
+	 * @return current points
+	 */
+	public int getPoints() {
+		return points;
+	}
 
-        /**
-         *
-         * @return if the player has enough points for
-         *          a victory
-         */
-        public boolean isVictory() {
-            return points >= 10;
-        }
+	/**
+	 *
+	 * @return if the player has enough points for a victory
+	 */
+	public boolean isVictory() {
+		return points >= 10;
+	}
 }
