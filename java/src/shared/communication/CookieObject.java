e6e4972f5a8403eef3da3b01e48a1c3a5b4f21f5
Candice Davis <candicedavis222@gmail.com>
2014-07-30 21:32:10 -0600

    cookieObject

---
+++ java/src/shared/communication/CookieObject.java
@@ -0,0 +1,49 @@
+package shared.communication;
+
+public class CookieObject {
+	
+	private String username;
+	private String password;
+	private int ID;
+	private int gameID;
+	
+	public CookieObject() {
+		
+	}
+
+	public CookieObject(String username, String password, int ID, int gameID) {
+		
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public int getGameID() {
+		return gameID;
+	}
+
+	public void setGameID(int gameID) {
+		this.gameID = gameID;
+	}
+}
