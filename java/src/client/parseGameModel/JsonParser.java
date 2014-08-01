d49d6f28683e0ff4e4ad9e64b6b2672c20aad824
Candice Davis <candicedavis222@gmail.com>
2014-07-31 19:56:16 -0600

    doParse

--- java/src/client/parseGameModel/JsonParser.java
+++ java/src/client/parseGameModel/JsonParser.java
@@ -1,13 +1,107 @@
 package client.parseGameModel;
 
-import org.json.JSONObject;
+import game.GameModel;
+
+import org.json.*;
+
 
 
 public class JsonParser {
 	
 	JSONObject jsonObject;
+	GameModel game = new GameModel();
+	
 	public JsonParser(String jsonString) {
-		jsonObject = new JSONObject(jsonString);
+		
+		this.jsonObject = new JSONObject(jsonString);		
+	}
+	
+	public GameModel doParse() {
+		
+		parsePlayers();
+		parseBoard();
+		parseBank();
+		parseGameHistory();
+		parseTurnTracker();
+		parseTradeOffer();
+		parseVersion();
+		parseWinner();
+		parseId();
+		parseRandomHexes();
+		parseRandomNumbers();
+		parseRandomPorts();
+		parseCheckDiscard();
+		parseGameName();
+		
+		return game;	
+	}
+	
+	public void parsePlayer() {
+		
+		String username = player.getString("username");
+		System.out.println(username);
+	}
+	
+	public void parsePlayers() {
+		
+		JSONArray players = jsonObject.getJSONArray("players");
+		JSONObject player = players.getJSONObject(0);
+		
+		
+	}
+	
+	public void parseBoard() {
+		
+	}
+	
+	public void parseBank() {
+		
+	}
+	
+	public void parseGameHistory() {
+		
+	}
+	
+	public void parseTurnTracker() {
+		
+	}
+	
+	public void parseTradeOffer() {
+		
+	}
+	
+	public void parseVersion() {
+		
+	}
+	
+	public void parseWinner() {
+		
+	}
+	
+	public void parseId() {
+		
+	}
+	
+	public void parseRandomHexes() {
+		
+	}
+	
+	public void parseRandomNumbers() {
+		
+	}
+	
+	public void parseRandomPorts() {
+		
+	}
+	
+	public void parseCheckDiscard() {
+		
+	}
+	
+	public void parseGameName() {
 		
 	}
 }
+
+
+
