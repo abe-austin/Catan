12353bcfc6eec55463ac476726c7f1b3ab9875a0
cjkirk09 <cjkirk09@gmail.com>
2014-08-01 19:18:41 -0600

    merge

--- java/src/client/serverProxy/ServerResponseConverter.java
+++ java/src/client/serverProxy/ServerResponseConverter.java
@@ -4,6 +4,7 @@ import client.data.GameInfo;
 import client.data.PlayerInfo;
 import client.parse.DoParse;
 import client.parseGameModel.JsonParser;
+import client.parseGameModel.ParseGameInfo;
 
 import com.google.gson.Gson;
 
@@ -48,31 +49,33 @@ public class ServerResponseConverter {
 	}
 	
 	public void convertGameInfo(ServerResponse response) {
-		if(response.getCode() == 200) {
-			response.setBody(gson.fromJson((String)response.getBody(), ArrayList.class));
-			ArrayList<GameInfo> games = (ArrayList<GameInfo>)response.getBody();
-			ArrayList<GameInfo> gameInfo = new ArrayList<GameInfo>();
-			for(Object game : games) {
-				String test = gson.toJson(game);
-				GameInfo info = gson.fromJson(test, GameInfo.class);
-				ArrayList<PlayerInfo> playerInfoList = new ArrayList<PlayerInfo>();
-				info.resetPlayer();
-				for(Object player : info.getPlayers()) {
-					String playerString = gson.toJson(player);
-					PlayerInfo playerInfo = gson.fromJson(playerString, PlayerInfo.class);
-					playerInfo.setColor(playerInfo.getColor());
-					playerInfoList.add(playerInfo);
-				}
-				info.setPlayerInfo(playerInfoList);
-				gameInfo.add(info);
-			}
-			response.setBody(gameInfo);
-			return;
-		}
-		else {
-			ArrayList<GameInfo> gameInfo = new ArrayList<GameInfo>();
-			response.setBody(gameInfo);
-			return;
-		}
+            ParseGameInfo parseGameInfo = new ParseGameInfo((String)response.getBody());
+            response.setBody(parseGameInfo.doParse());
+//		if(response.getCode() == 200) {
+//			response.setBody(gson.fromJson((String)response.getBody(), ArrayList.class));
+//			ArrayList<GameInfo> games = (ArrayList<GameInfo>)response.getBody();
+//			ArrayList<GameInfo> gameInfo = new ArrayList<GameInfo>();
+//			for(Object game : games) {
+//				String test = gson.toJson(game);
+//				GameInfo info = gson.fromJson(test, GameInfo.class);
+//				ArrayList<PlayerInfo> playerInfoList = new ArrayList<PlayerInfo>();
+//				info.resetPlayer();
+//				for(Object player : info.getPlayers()) {
+//					String playerString = gson.toJson(player);
+//					PlayerInfo playerInfo = gson.fromJson(playerString, PlayerInfo.class);
+//					playerInfo.setColor(playerInfo.getColor());
+//					playerInfoList.add(playerInfo);
+//				}
+//				info.setPlayerInfo(playerInfoList);
+//				gameInfo.add(info);
+//			}
+//			response.setBody(gameInfo);
+//			return;
+//		}
+//		else {
+//			ArrayList<GameInfo> gameInfo = new ArrayList<GameInfo>();
+//			response.setBody(gameInfo);
+//			return;
+//		}
 	}
 } 
